c2483fad3d3ab26832d185b48bba310ec1bc6ded
igor18076 <45477142+igor18076@users.noreply.github.com>
2023-04-06 01:26:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 // Cross-Origin Resource Sharing
-app.use(cors({origin: ['http://localhost:3000'], credentials: true}))
+app.use(cors({origin: ['http://45.9.42.121:3000'], credentials: true}))
 // middleware для работы с json
 app.use(express.json())
 // middleware для статики (img, css)
